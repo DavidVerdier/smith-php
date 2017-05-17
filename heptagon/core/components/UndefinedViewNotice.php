2e9a77ae6905613fb5c2eb54461a18992b460e64
EtienneOrlhac <etienneorlhac@gmail.com>
2017-05-17 13:41:26 +0200

    Update UndefinedViewNotice.php

--- heptagon/core/components/UndefinedViewNotice.php
+++ heptagon/core/components/UndefinedViewNotice.php
@@ -1,8 +1,7 @@
 <?php
 class UndefinedViewNotice extends Component {
     public function run(App $app) {
-        $doc = $app->getDocument();
-        return new Paragraph('DefaultView : no view has been defined before application execution.');
+        return new TextNode('DefaultView : no view has been defined before application execution.');
     }
 }
 ?>

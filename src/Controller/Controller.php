8651debf4cea198785bf1169fc16ca5d90e167c1
EtienneOrlhac <etienneorlhac@gmail.com>
2017-10-25 04:33:30 +0200

    -

---
+++ src/Controller/Controller.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Etienne
+ * Date: 20-Oct-17
+ * Time: 3:07
+ */
+
+namespace Smith\Controller;
+
+
+abstract class Controller {
+
+
+}
---
+++ src/Http/AbstractResponse.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Etienne
+ * Date: 21-Oct-17
+ * Time: 11:50
+ */
+
+namespace Smith\Http;
+
+
+interface AbstractResponse {
+
+    /**
+     * Sets the HTTP status code and message to be sent to the client.
+     * @param int $status
+     * @param string $httpVersion
+     */
+    public function setStatus(int $status, string $httpVersion);
+
+    /**
+     * Sets a response header.
+     * @param string $key
+     * @param string $value
+     */
+    public function setHeader(string $key, string $value);
+
+    /**
+     * Writes / Overwrites the contents of the response body.
+     * @param string $content
+     */
+    public function setBody(string $content);
+
+    /**
+     * Appends content to the reponse body.
+     * @param string $content
+     */
+    public function append(string $content);
+
+    /**
+     * Prepends content to the response body.
+     * @param string $content
+     */
+    public function prepend(string $content);
+
+    /**
+     * Sends only the headers for this response.
+     * Once sent they cannot be re-sent and modifications will be ignored.
+     */
+    public function sendHeaders();
+
+    /**
+     * Sends the response. If headers have already been sent, only sends the body.
+     * Once sent it cannot be resent and modifications will be ignored.
+     */
+    public function send();
+}
